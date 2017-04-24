365db53f307dab23b9a4558bbe87ec6e79af1776
robin.you@telecom-bretagne.eu <robin.you@telecom-bretagne.eu>
2017-04-24 12:34:05 +0000

    job.sh actualized

--- Project2/Programs/job.sh
+++ Project2/Programs/job.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#PBS -N test
+#PBS -N q5test2bis 
 #PBS -q training
 #PBS -A imf_lille-tma4280
 #PBS -W group_list=imf_lille-tma4280
 #PBS -l walltime=00:15:00
-#PBS -l select=2:ncpus=20:mpiprocs=16:ompthreads=1
+#PBS -l select=2:ncpus=20:mpiprocs=8:ompthreads=4
 
 cd $PBS_O_WORKDIR
 
-mpirun ./poisson 4 
+echo $PBS_O_WORKDIR
+
+module load gcc/6.3.0
+module load openmpi/2.0.1
+
+mpirun ./poisson 1024 4 
